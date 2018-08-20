682dcd55b7b804460e29498eb17a830f3abd373b
MIDORIBIN <aquarion123@gmail.com>
2018-08-20 12:51:38 +0900

    add: or検索できるように

--- src/service/request-service.ts
+++ src/service/request-service.ts
@@ -1,6 +1,12 @@
 import store from '@/store.ts';
 
 export default function() {
-  console.log(store.state.searchKeyword);
-  window.open('https://qiita.com/search?q=' + store.state.searchKeyword);
+  const searchKeyword = andOr(store.state.searchKeyword);
+  window.open('https://qiita.com/search?q=' + searchKeyword);
+}
+function andOr(keyword: string): string {
+  if (store.state.andOr === 'AND') {
+    return keyword;
+  }
+  return keyword.split(' ').join(' OR ');
 }
