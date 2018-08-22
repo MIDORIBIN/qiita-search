5730f3ca9f81c89af76019b1ffdaa2faf1de2146
MIDORIBIN <aquarion123@gmail.com>
2018-08-22 17:26:19 +0900

    add: buildしたファイルの追加
    github pages で公開するためにdocフォルダにbuildしたファイルを追加
    また公開するにあったって設定ファイルを追加

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  outputDir: 'doc',
+  baseUrl: './',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
